116ffcc4b515ab6fb76333f2754aabf2ea273dde
mariar2006 <maria.r2006@aluno.ifsc.edu.br>
2024-12-01 17:01:04 -0300

    .

---
+++ listaPHP/Qst1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qst1</title>
+</head>
+<body>
+
+   // Quais são as tags usadas para definição de código PHP?
+//<?php 
+//?>
+
+
+
+
+</body>
+</html>
