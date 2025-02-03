0c4117a9cb5babd9c42397ca91be937539436e57
unknown <maria.r2006@aluno.ifsc.edu.br>
2025-02-03 08:50:09 -0300

    cadastro

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,7 +1,21 @@
-<form action="rota.php" method ="post">
-    <input type="text" name="nome" placeholder="Nome">
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="Password" placeholder="password">
-    <input type="text" name="op" value=cadastrar_usuario hidden>
-    <input type="submit" value="Cadastrar">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+
+<body>
+    <fieldset>
+        <form action="PessoaController.php" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+            <input type = "text" name= "acao" value= "cadastrar" hidden>
+            <input type="submit" value="Adicionar">
+        </form>
+    </fieldset>
+</body>
+</html>
